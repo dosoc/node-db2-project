509cce3dff4c8450c1fb15e4090903b64cd8f960
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-19 22:08:17 -0500

    clean up

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl => {
-    tbl.increments('id');
-    tbl.string('vin').notNullable().unique();
-    tbl.string('make').notNullable();
-    tbl.string('model').notNullable();
+    tbl.increments();
+    tbl.string('vin', 17).notNullable().unique();
+    tbl.string('make', 128).notNullable();
+    tbl.string('model', 128).notNullable();
     tbl.integer('mileage').notNullable();
-    tbl.string('title');
-    tbl.string('transmission');
+    tbl.string('title', 128);
+    tbl.string('transmission', 128);
   })
 };
 
