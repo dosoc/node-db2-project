905a9329f5403f877f17e93e7d5cd2863c98842c
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-07-19 22:03:58 -0500

    stretch

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,41 @@
 // STRETCH
+
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 123456,
+        title: 'clean'
+    },
+    {
+        vin: '11111111111111112',
+        make: 'chevy',
+        model: 'cavilier',
+        mileage: 123456,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '11111111111111113',
+        make: 'toyota',
+        model: 'corola',
+        mileage: 12346,
+        title: 'clean'
+    },
+    {
+        vin: '11111111111111114',
+        make: 'ford',
+        model: 'f-250',
+        mileage: 1256,
+        title: 'salvage',
+        transmission: 'auto'
+    },
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
